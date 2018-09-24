6cf70172c44c44fb2dcd83f1c72330b1bc1ed23f
mrApptastic <henrikbeske@gmail.com>
2018-09-24 23:40:40 +0200

    Expanded and cleaned application.

--- scripts/script.js
+++ scripts/script.js
@@ -7,24 +7,50 @@ app.run(function ($rootScope, mesageService) {
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        template : "<h2>Good Evening.</h2>",
+        template : "<header id='streetBandit' class='mrBox mrBounceIn'>" +
+"<div class='streetOverlay'>" +
+"<h1 class='aqui mrsRed mrH1 mrCenter mrMart2 mrNice'>" +
+"Henriks Portfolio" +
+"</h1>" +
+"</div>" +
+"<div>" +
+    "<div class='road'></div>" +
+"<img class='streetLeft'" + 
+     "alt='Street Left'" + 
+     "src='media/images/StreetLeft.png'" + 
+     "/>" +
+"<img class='streetRight'" + 
+     "alt='Street Right'" + 
+     "src='media/images/StreetRight.png'" +  
+     "/>" +
+"<img class='himSelf'" + 
+     "alt='Ham Selv'" + 
+     "src='media/images/Himself.png'" + 
+     "/>" +
+"</div>" +
+"</header>",
+    controller: "mainController"    
     })
     .when("/london", {
         template : "<h2>Hello. {{msg}}</h2>",
-        controller : "londonCtrl"
+        controller : "londonController"
     })
     .when("/paris", {
         template : "<h2>Hi there. <span ng-bind='msg'></span></h2><h1>Hide the DIV: <input type='checkbox' ng-model='myCheck'></h1><div ng-hide='myCheck' class='theDiv'></div>" +
 		"<ul><li ng-repeat='p in persons' ng-bind='p.Name'></ul>",
-        controller : "parisCtrl"
+        controller : "parisController"
     });
 });
 
-app.controller("londonCtrl", function ($scope, $rootScope) {
+app.controller("mainController", function ($scope, $rootScope) {
+
+});
+
+app.controller("londonController", function ($scope, $rootScope) {
     $scope.msg = "I love London" + $rootScope.msg;
 });
 
-app.controller("parisCtrl", function ($scope, $http, htmlService) {
+app.controller("parisController", function ($scope, $http, htmlService) {
     $scope.msg = "I love Paris" + htmlService.hello("<h1>Hej hej</h1>") +
 	htmlService.getAll()
 	        .then(function (result) {
@@ -35,6 +61,10 @@ app.controller("parisCtrl", function ($scope, $http, htmlService) {
 				console.log(result);
                 alert("Totalt øv fejl!");
             });
+});
+
+app.controller("locationController", function ($scope, $location) {
+    $scope.location = $location.path();
 });
 
 app.service('mesageService', function() {
