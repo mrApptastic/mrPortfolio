7e5d4c416562808405e7cf9fa6d6153126c988a1
mrApptastic <30831839+mrApptastic@users.noreply.github.com>
2021-05-28 15:03:15 +0200

    AdminApp: Portfolio controller.

---
+++ portfolioAdminApp/Controllers/PortfolioController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using portfolioAdminApp.Data;
+using portfolioAdminApp.Models;
+
+namespace portfolioAdminApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class PortFolioController : ControllerBase
+    {
+
+        private readonly ILogger<PortFolioController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public PortFolioController(ILogger<PortFolioController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet("Quals")]
+        public async Task<ActionResult<ICollection<Translation>>> GetQuals()
+        {
+            var uha = await _context.PortfolioQualificationTranslations.ToListAsync();
+            return Ok(uha);
+        }   
+    }
+}
