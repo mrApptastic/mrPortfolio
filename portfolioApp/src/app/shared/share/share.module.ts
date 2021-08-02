ac7fa0081e59b6fd1aadd7d062771c3b16280c0e
mrApptastic <30831839+mrApptastic@users.noreply.github.com>
2021-08-02 22:47:37 +0200

    PortfolioApp: Share Module and cv styles.

---
+++ portfolioApp/src/app/shared/share/share.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MrMr2Module } from 'mr-mr2';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MrMr2Module
+  ]
+})
+
+export class ShareModule { }
