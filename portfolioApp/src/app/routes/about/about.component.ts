6067ec7c67838c7b5a286f0127b0704a29ce2adc
mrApptastic <30831839+mrApptastic@users.noreply.github.com>
2021-11-16 00:43:19 +0100

    PortfolioApp: text change in about section.

--- portfolioApp/src/app/routes/about/about.component.ts
+++ portfolioApp/src/app/routes/about/about.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
   typeSettings: any = {
     text : new Array(
-      "Til dagligt er jeg Henrik Beske, et helt almindeligt familiemenneske. Men idet jeg tager afsted på arbejde, forvandler jeg mig til mrApptastic, en superheroisk webudvikler med gazelleblod i blodbanerne."
+      "Til dagligt er jeg Henrik Beske, et helt almindeligt familiemenneske. Men idet jeg tager afsted til arbejde, forvandler jeg mig til mrApptastic, en superheroisk webudvikler med gazelleblod fossende gennem blodbanerne."
     ),
     speed : 20,
     index : 0,
