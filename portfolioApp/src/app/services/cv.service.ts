14ddd508f3a5db9e023f2d56838608761e3bfba9
mrApptastic <henrikbeske@gmail.com>
2021-12-11 15:50:08 +0100

    PortfolioApp: Font and image stuff.

--- portfolioApp/src/app/services/cv.service.ts
+++ portfolioApp/src/app/services/cv.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { jsPDF } from 'jspdf';
+import { Lato } from './lato';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,15 @@ export class CvService {
     const doc = new jsPDF();
     // doc.addFont('Ringbearer', '', 'normal');
     // doc.setFont('Courier');
+    var img = new Image()
+    // img.src = "assets/images/Himself.png";
+    // doc.addImage(img, 'png', 10, 78, 12, 15);
 
+    jsPDF.API.events.push(['addFonts', function() {
+      // doc.addFileToVFS('Lato-Regular-normal.ttf', Lato);
+      // doc.addFont('Lato-Regular-normal.ttf', 'Lato-Regular', 'normal');
+    }]);
+    doc.setFont('Courier');
     doc.text("Hej lille tulipan", 10, 10);
     doc.save("Uha.pdf");
   }
